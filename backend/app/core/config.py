f08bc180a8d8c53ddaffe3945780ad70cf013e88
yoonlee201 <93956761+yoonlee201@users.noreply.github.com>
2026-03-01 17:52:27 -0500

    make backend port changable

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,6 +7,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     
     FRONTEND_URL = settings.FRONTEND_URL
+    BACKEND_PORT = settings.BACKEND_PORT
     
     OPENAI_API_KEY = settings.OPENAI_API_KEY
     OPENAI_MODEL = settings.OPENAI_MODEL
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,7 @@
 from app import create_app
+from app.core.config import config
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001, debug=True)
+    app.run(host="0.0.0.0", port=config.BACKEND_PORT, debug=True)
